07f11e19c08499073fe87ff5e39a69dc97f98b6b
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-10-05 18:04:15 +0300

    create t4

---
+++ task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > thirdNumber)
+    {
+        Console.WriteLine("это максимальное число: " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("это максимальное число: " + thirdNumber);
+    }
+}
+
+else if (secondNumber > thirdNumber)
+{
+    Console.WriteLine("это максимальное число: " + secondNumber);
+}
+else
+{
+    Console.WriteLine("это максимальное число: " + thirdNumber);
+}
