13d6fc05cc1bc8f73881b06175a46ef4c1eeb928
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-10-05 18:10:32 +0300

    create task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число: ");
+int number = int.Parse(Console.ReadLine());
+int firstNumber = 1;
+
+
+while (firstNumber <= number)
+{
+    if (firstNumber % 2 == 0)
+        Console.Write(firstNumber + ", ");
+    firstNumber++;
+}
